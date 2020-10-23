33e365a46a60f989d36889771c146d22049aee9a
Arturo Simon <artysimon@gmail.com>
2020-10-23 08:39:20 -0400

    Add toggle to question's parent container

--- js/main.js
+++ js/main.js
@@ -5,16 +5,29 @@ const answers = document.getElementsByClassName("faq-section__answer");
 function displayQuestion(event) {
     event.preventDefault();
 
-    // Hide all answers by remove the open class
-    for (i=0; i < answers.length; i++) {
-        answers[i].parentElement.classList.remove("faq-section--open");
-    }
+    let parentSection = this.parentElement;
 
-    // Add class to parent container of question
-    this.parentElement.classList.add("faq-section--open");
+    if (parentSection.classList.contains("faq-section--open")) {
+        // This current element's parent should
+        // be the only open section
+
+        // Just toggle it off
+        parentSection.classList.toggle("faq-section--open");
+
+    } else {
+        // Otherwise a different parent is open
+        // so close all of them
+        for (i=0; i < answers.length; i++) {
+            answers[i].parentElement.classList.remove("faq-section--open");
+        }
+
+        // Add class to current parent container of question
+        parentSection.classList.add("faq-section--open");
+    }
 }
 
 
+// Add click event listener to each element
 for (i=0; i < questions.length; i++) {
     questions[i].addEventListener("click", displayQuestion);
 }
